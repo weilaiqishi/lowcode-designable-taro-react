e321ab6d5feed32be736d97b412f1dc0fbd49b70
925577835 <925577835@qq.com>
2022-05-30 15:04:23 +0800

    base

---
+++ designable-basicPlayground/README.md
@@ -0,0 +1,3 @@
+# @designable/playground
+
+从 [alibaba/designable](https://github.com/alibaba/designable) Monorepo 中抽出来的 basic playground
---
+++ designable-basicPlayground/config/webpack.base.ts
@@ -0,0 +1,73 @@
+import path from 'path'
+import fs from 'fs-extra'
+import MiniCssExtractPlugin from 'mini-css-extract-plugin'
+//import { getThemeVariables } from 'antd/dist/theme'
+
+export default {
+  mode: 'development',
+  devtool: 'inline-source-map', // 嵌入到源文件中
+  stats: {
+    entrypoints: false,
+    children: false,
+  },
+  entry: {
+    playground: path.resolve(__dirname, '../src/main'),
+  },
+  output: {
+    path: path.resolve(__dirname, '../dist'),
+    filename: '[name].[hash].bundle.js',
+  },
+  resolve: {
+    modules: ['node_modules'],
+    extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
+  },
+  externals: {
+    // '@formily/reactive': 'Formily.Reactive',
+    react: 'React',
+    'react-dom': 'ReactDOM',
+    moment: 'moment',
+    antd: 'antd',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: [
+          {
+            loader: require.resolve('ts-loader'),
+            options: {
+              transpileOnly: true,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, require.resolve('css-loader')],
+      },
+      {
+        test: /\.less$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          { loader: 'css-loader' },
+          {
+            loader: 'less-loader',
+            options: {
+              // modifyVars: getThemeVariables({
+              //   dark: true // 开启暗黑模式
+              // }),
+              javascriptEnabled: true,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.html?$/,
+        loader: require.resolve('file-loader'),
+        options: {
+          name: '[name].[ext]',
+        },
+      },
+    ],
+  },
+}
