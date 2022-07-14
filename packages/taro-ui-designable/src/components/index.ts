48b74e7eb6641840f08cc89f7050db1c23be382a
925577835 <925577835@qq.com>
2022-07-14 20:34:52 +0800

    refactor(taro-ui): delete taro-ui-formily

--- packages/taro-ui-designable/src/components/index.ts
+++
@@ -1,9 +0,0 @@
-export * from './Field'
-export * from './Form'
-export * from './Input'
-export * from './InputNumber'
-export * from './Rate'
-export * from './Switch'
-export * from './WidgetBase'
-
-
--- packages/taro-ui-designable/src/widgets/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './PreviewWidget'
-export * from './SchemaEditorWidget'
